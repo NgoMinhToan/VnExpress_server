1a67eeed9e6666ae2190d19635470a4833fbfcfc
Ngo Minh Toan <mtoan2610@gmail.com>
2022-07-12 21:53:52 +0700

    init push

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2yv#y@u5hn!9yfjqxwbz((++2!(gcn&#_#b0+j2e@)ga96c#$3'
+SECRET_KEY = env('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
