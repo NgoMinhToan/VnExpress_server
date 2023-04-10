f50c452a9ad11806f34e6c444a0d75344d27cab5
NgoMinhToan <mtoan2610@gmail.com>
2023-04-10 11:13:43 +0700

    7

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('APP_SECRET_KEY', default='for_development_only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
